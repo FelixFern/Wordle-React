ef46579434a6fc1174b731c04e5105ac32e4ab24
Felix Fernando <felixzhuang1@gmail.com>
2022-04-21 20:33:53 +0700

    keyboard support W.I.P

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ let keyboardColor = [['n', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n', 'n'],
 function App() {
 	useEffect(() => {
 		document.title = ("Wordle Recreated")
+		window.addEventListener('keypress', e => {
+			keyboardInput(e.key)
+			console.log(e.key)
+		});
 	},[])
 	const [currentWord, setWord] = useState([])
 	const [currentLine, setLine] = useState(0)
@@ -22,13 +26,69 @@ function App() {
 	const guessing_word = "tests"
 
 	const firstRow = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p']
-	
-
     const secondRow = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l']
-
-
     const thirdRow = ['enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<']
 
+	function keyboardInput(alphabet) {
+		if(alphabet != "Enter" && alphabet != "<") {
+			if(currentLine < 5 && currentWord.length < 5) {
+				setWord((word) => [...word, [alphabet.toUpperCase()]])
+			}
+		}
+		else if(alphabet == "<") {
+			const word_copy = currentWord
+			word_copy.splice(currentWord.length - 1, 1)
+			setWord((word) => [...word_copy])
+		}
+		else if(alphabet == "Enter") {
+
+			if(currentWord.length == 5) {
+				let colors = ""
+				let color = ""
+				currentWord.map((word, i) => {
+					color = ""
+					let available = false
+					for(let j = 0; j < 5; j++) {
+						if(word == guessing_word[j].toUpperCase() && i == j) {
+							color = "G"
+							let {bool, row, ind} = setKeyboardColor(word)
+							if(bool) {
+								keyboardColor[row][ind] = "g"
+							}
+							console.log(keyboardColor)
+							// console.log(word + "x" + guessing_word[j] + "xG")
+							break
+						}
+						else if(word == guessing_word[j].toUpperCase() && i != j) {
+							available = true
+							let {bool, row, ind} = setKeyboardColor(word)
+							if(bool && keyboardColor[row][ind] != "g") {
+								keyboardColor[row][ind] = "y"
+							}
+							// console.log(word + "x" + guessing_word[j] + "xY")
+							console.log(keyboardColor)
+							color = "Y"
+						}
+						else if(available == false){
+							let {bool, row, ind} = setKeyboardColor(word)
+							if(bool) {
+								keyboardColor[row][ind] = "N"
+							}
+							console.log(keyboardColor)
+							// console.log(word + "x" + guessing_word[j] + "xN")
+							color = "N"
+						}
+					}
+					colors += color
+				})
+				word_list[currentLine] = currentWord
+				color_list[currentLine] = colors
+				colors = ""
+				setLine(currentLine + 1)											
+				setWord(word => [] )
+			}
+		}
+	}
 	function setKeyboardColor(alphabet) {
 		let bool = false
 		let row = 0
